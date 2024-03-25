c4c32864a44237980ee7eefaf3d82f7eb84d9920
VictorGob <vgordaliza@gmail.com>
2024-03-25 11:00:55 +0100

    app's schema created

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::fairing::AdHoc;
+
+#[get("/<url_id>")]
+fn shortener(url_id: &str) -> String {
+    format!("Hello, world! -> {}", url_id)
+}
+
+#[post("/", data = "<url>")]
+fn short_creation(url: &str) -> String {
+    format!("Hello, world! -> {}", url)
+}
+
+#[get("/")]
+fn landing() -> String {
+    "Hello, world!".to_string()
+}
+
+fn check_database() -> Result<(), String> {
+    todo!()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(AdHoc::try_on_ignite(
+            "Database Connection Check",
+            |rocket| async {
+                match check_database() {
+                    Ok(_) => Ok(rocket),
+                    Err(_) => Err(rocket),
+                }
+            },
+        ))
+        .mount("/", routes![shortener, landing, short_creation])
+}
