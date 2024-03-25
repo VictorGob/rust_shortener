49b40f4fd2ae0e28657a8c4985d46c8c566b229d
VictorGob <vgordaliza@gmail.com>
2024-03-25 14:51:52 +0100

    WIP: added postges and other crates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fairing::AdHoc;
+use postgres::{Client, NoTls};
+use rocket::http::Status;
+use url::Url;
 
 #[get("/<url_id>")]
 fn shortener(url_id: &str) -> String {
@@ -9,8 +11,9 @@ fn shortener(url_id: &str) -> String {
 }
 
 #[post("/", data = "<url>")]
-fn short_creation(url: &str) -> String {
-    format!("Hello, world! -> {}", url)
+fn short_creation(url: &str) -> Result<String, Status> {
+    let id = &nanoid::nanoid!(6);
+    let p_url = Url::parse(&url).map_err(|_| Status::UnprocessableEntity)?;
 }
 
 #[get("/")]
@@ -20,21 +23,40 @@ fn landing() -> String {
 
 fn check_database() -> Result<(), String> {
     println!("Checking database connection...");
+    let mut client = match Client::connect(
+        "host=localhost user=postgres password=local_url_shortener",
+        NoTls,
+    ) {
+        Ok(client) => client,
+        Err(e) => {
+            return Err(format!("Error connecting to database: {}", e));
+        }
+    };
+
+    let query = "SELECT EXISTS (
+        SELECT 1
+        FROM   information_schema.tables 
+        WHERE  table_name = 'urls_data'
+     );";
+    match client.query_one(query, &[]) {
+        Ok(row) => {
+            let exists: bool = row.get(0);
+            if exists {
+                println!("The table 'urls_data' exists.");
+            } else {
+                return Err("The table 'urls_data' does not exist.".to_string());
+            }
+        }
+        Err(e) => {
+            return Err(format!("Error executing query: {}", e));
+        }
+    }
+
     Ok(())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .configure(rocket::Config::figment())
-        .attach(AdHoc::try_on_ignite(
-            "Database Connection Check",
-            |rocket| async {
-                match check_database() {
-                    Ok(_) => Ok(rocket),
-                    Err(_) => Err(rocket),
-                }
-            },
-        ))
-        .mount("/", routes![shortener, landing, short_creation])
+    let _ = check_database();
+    rocket::build().mount("/", routes![shortener, landing, short_creation])
 }
